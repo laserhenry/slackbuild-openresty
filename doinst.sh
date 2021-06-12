03c4240ef83ae5f61dfaa0f68a7a3c2a7b200400
laserhenry <laserhenry@gmail.com>
2021-06-12 16:09:52 +0100

    0.1.1ersion update openresty to 1.19.3.2

--- doinst.sh
+++ doinst.sh
@@ -13,14 +13,23 @@ config() {
 
 preserve_perms() {
   NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  if [ -e $OLD ]; then
-    cp -a $OLD ${NEW}.incoming
-    cat $NEW > ${NEW}.incoming
-    mv ${NEW}.incoming $NEW
+  OLD="$(dirname ${NEW})/$(basename ${NEW} .new)"
+  if [ -e ${OLD} ]; then
+    cp -a ${OLD} ${NEW}.incoming
+    cat ${NEW} > ${NEW}.incoming
+    mv ${NEW}.incoming ${NEW}
   fi
-  config $NEW
+  config ${NEW}
 }
 
-preserve_perms etc/dehydrated/config.new
-preserve_perms etc/cron.d/dehydrated.new
+preserve_perms etc/rc.d/rc.nginx.new
+config etc/logrotate.d/nginx.new
+config etc/nginx/fastcgi_params.new
+config etc/nginx/fastcgi.conf.new
+config etc/nginx/mime.types.new
+config etc/nginx/nginx.conf.new
+config etc/nginx/koi-utf.new
+config etc/nginx/koi-win.new
+config etc/nginx/scgi_params.new
+config etc/nginx/uwsgi_params.new
+config etc/nginx/win-utf.new
